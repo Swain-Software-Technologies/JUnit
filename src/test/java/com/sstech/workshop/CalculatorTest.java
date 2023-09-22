55ad9a46abae154ed9d2a58303e7d19b5d419cf7
trinath.swain <trinath.967.swain@gmail.com>
2023-09-22 12:32:40 +0530

    WORKSHOP-101: Added CalculatorTest calss

---
+++ src/test/java/com/sstech/workshop/CalculatorTest.java
@@ -0,0 +1,20 @@
+/*
+ * @CopyRight to SSLab Pvt. Ltd. 2020. You should not disclose the information outside .
+ * Otherwise Terms & Condition will be apply .
+ */
+package com.sstech.workshop;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author STRINATH Sep 22, 2023
+ * 
+ * @Description : 
+ */
+public class CalculatorTest {
+
+	@Test
+	void testCalculator() {
+		System.out.println("Calculator tested Successfull");
+	}
+}
