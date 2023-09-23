a739e45a7eef92e1e3b093a31affeaca16513fec
trinath.swain <trinath.967.swain@gmail.com>
2023-09-23 18:28:57 +0530

    WORKSHOP-101: Added test cases in CalculatorTest class

--- src/main/java/com/sstech/workshop/model/Calculator.java
+++ src/main/java/com/sstech/workshop/model/Calculator.java
@@ -11,6 +11,21 @@ package com.sstech.workshop.model;
  */
 public class Calculator {
 
+	public int add(int num1, int num2) {
+		int result = num1 + num2;
+		return result;
+	}
+	
+	public int sub(int num1, int num2) {
+		int result = num1 - num2;
+		return result;
+	}
+	
+	public int mul(int num1, int num2) {
+		int result = num1 * num2;
+		return result;
+	}
+	
 	public int doDivision(int dividend, int divisor) {
 		return dividend / divisor;
 	}
--- src/test/java/com/sstech/workshop/model/CalculatorTest.java
+++ src/test/java/com/sstech/workshop/model/CalculatorTest.java
@@ -5,8 +5,8 @@
 package com.sstech.workshop.model;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
 
-import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,16 +18,51 @@ import org.junit.jupiter.api.Test;
  * @Description : 
  */
 public class CalculatorTest {
-
-	@Test
-	void testCalculator() {
-		System.out.println("Calculator tested Successfull");
+	
+	
+	Calculator calculator;
+	
+	@BeforeEach
+	public void setup() {
+		calculator = new Calculator();
 	}
 	
 	@Test
+	public void testAdd_ForPositiveNumber() {
+		int result = calculator.add(10, 23);
+		assertEquals(33, result);
+	}
+	
+	@Test
+	public void testAdd_ForNegativeNumber() {
+		int result = calculator.add(-10, -23);
+		assertEquals(-33, result);
+	}
+	
+	@Test
+	public void testAdd_ForPositiveNegativeNumber() {
+		int result = calculator.add(10, -23);
+		assertEquals(-13, result);
+	}
+	
+	
+	public void testSub() {
+		fail("Not yet implemented");
+	}
+	
+	public void testMul() {
+		fail("Not yet implemented");
+	}
+	
+	
+	@Test
 	public void testDoDivision() {
-		Calculator calculator = new Calculator();
 		int result = calculator.doDivision(4, 2);
 		assertEquals(2, result, "4/2 did not produce 2");
+	}
+	
+	@AfterEach
+	public void tierDown() {
+		calculator = null;
 	}
 }
